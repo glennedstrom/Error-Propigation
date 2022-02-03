c8058733a3764c940a52b97a2537cefdba486ba6
glennedstrom <edstrom.glenn@gmail.com>
2022-02-03 17:12:10 -0600

    working with x*2 and 2*x; need to double-check answers; sig-figs are off, but in progress

--- error-propigation.py
+++ error-propigation.py
@@ -1,5 +1,7 @@
 import math
+from math import pi # in case you want to reference pi while calculating
 import warnings
+import re
 
 #error propagation class
 class err():
@@ -11,67 +13,153 @@ class err():
     Overloaded math functions to auto-calculate error:
     * / + - **
 
+    *sigfigs don't work yet; they are commented out
     """
-    used_funct = []
+    vars = {} # convert user's letters to dictionary indexing syntax and replace the values in the formula with str.replace()
 
-    def __init__(self, val, err):
-        self.val = val
-        self.err = err
+    def __init__(self, val, err, sig=-1):
+
+        if sig == -1: #significant figures
+            self.sig = self.sigfigs(val)
+        else:
+            self.sig = sig
+
+        # warn if the input wasn't a string originally
+        self.str = str(val)
+        self.val = float(val)   # number
+        self.err = float(err)   # plus/minus error
+
+    def sigfigs(self, val): #do this before the number is a float, otherwise the number may have a different amount of sig figs than intended
+        if '.' in val:
+            return len(val.lstrip('0.').replace('.', ''))
+        else:
+            return len(val.strip('0'))
 
     # operation overloading
 
     def __add__(self, o):
         if type(o) != err:
             warnings.warn("WARNING: types do not match, autoconverting with o.err=0")
-            o = err(o, 0)
-        return err(self.val + o.val, math.sqrt(self.err**2 + o.err**2))
+            o = err(str(o), 0)
+
+        ans = self.val + o.val
+
+        #sig figs (numbers after decimal) # -1 to remove decimal point
+        sig = str(ans).index('.') + (min(len(str(self.val % 1)), len(str(o.val % 1))) - 1)
+
+        return err(str(ans), math.sqrt(self.err**2 + o.err**2), sig)
+    __radd__ = __add__
 
     def __sub__(self, o):
         if type(o) != err:
             warnings.warn("WARNING: types do not match, autoconverting with o.err=0")
-            o = err(o, 0)
-        return err(self.val - o.val, math.sqrt(self.err**2 + o.err**2))
+            o = err(str(o), 0)
+
+        ans = self.val - o.val
+
+        #sig figs (numbers after decimal) # -1 to remove decimal point
+        sig = str(ans).index('.') + (min(len(str(self.val % 1)), len(str(o.val % 1))) - 1)
+
+        return err(str(ans), math.sqrt(self.err**2 + o.err**2), sig)
+
+    def __rsub__(self, o):
+        if type(o) != err:
+            warnings.warn("WARNING: types do not match, autoconverting with o.err=0")
+            o = err(str(o), 0)
+
+        ans = o.val - self.val
+
+        return err(str(ans), math.sqrt(self.err**2 + o.err**2), -1)
 
     def __mul__(self, o):
         if type(o) != err:
             warnings.warn("WARNING: types do not match, autoconverting with o.err=0")
-            o = err(o, 0)
+            o = err(str(o), 0)
+
         ans = self.val * o.val
-        answer = math.sqrt((self.err/self.val)**2 + (o.err/o.val)**2)*ans
-        return err(ans, answer)
+        error = math.sqrt((self.err/self.val)**2 + (o.err/o.val)**2)*ans
+        sig = self.sigfigs(str(ans))
+        return err(str(ans), error, sig)
+    __rmul__ = __mul__
 
     def __truediv__(self, o):
         if type(o) != err:
             warnings.warn("WARNING: types do not match, autoconverting with o.err=0")
-            o = err(o, 0)
+            o = err(str(o), 0)
         ans = self.val / o.val
-        answer = math.sqrt((self.err/self.val)**2 + (o.err/o.val)**2)*ans
-        return err(ans, answer)
+        error = math.sqrt((self.err/self.val)**2 + (o.err/o.val)**2)*ans
+        sig = self.sigfigs(str(ans))
+        return err(str(ans), error, sig)
+
+    def __rtruediv__(self, o):
+        if type(o) != err:
+            warnings.warn("WARNING: types do not match, autoconverting with o.err=0")
+            o = err(str(o), 0)
+        ans =  o.val / self.val
+        error = math.sqrt((self.err/self.val)**2 + (o.err/o.val)**2)*ans
+        return err(str(ans), error)
 
     def __pow__(self, o):
         if type(o) != err: #exponents need to be exact
-            o = err(o, 0)
+            o = err(str(o), 0)
         elif o.err != 0:# x**n; where n needs to have no error
             warnings.warn("WARNING: your exponent needs to be exact; ignoring error...")
         ans = self.val ** o.val
         derivative = o.val*self.val**(o.val-1)
         answer = abs(derivative)*self.err
-        return err(ans, answer)
+        return err(str(ans), answer)
 
+    def __rpow__(self, o):
+        if type(o) != err: #exponents need to be exact
+            warnings.warn("WARNING: types do not match, autoconverting with o.err=0")
+            o = err(str(o), 0)
+        elif self.err != 0:# x**n; where n needs to have no error
+            warnings.warn("WARNING: your exponent needs to be exact; ignoring error...")
+
+        ans =  o.val ** self.val
+        derivative = self.val*o.val**(self.val-1)
+        answer = abs(derivative)*o.err
+        return err(str(ans), answer)
     #output formatting
     def __str__(self):
+
+        #decimals = len(str(self.val)) - self.sig-1#-1 for decimal point maybe
+        #print(decimals)
         return str(self.val) + " ± " + str(self.err)
 
 
+def main():
+    while True:
+        try:# for bad user input
+            var = input("Enter a variable; none to continue: ")
+            if var == '':
+                break
+
+            val = input(var + " = ")
+            e = input(var + " = " + val + " ± ")
+
+            err.vars[var] = err(val, e)
+
+        except Exception as e:
+            print(e)
+    print("\nType your equations below, ctrl-c to exit")
+    converted = ''
+    while True:
+        try:
+            eq = input()
+            if eq == '':#loop back to variable definition
+                break
+
+            converted = eq
+            pattern = re.compile(r'(' + '|'.join(sorted(err.vars.keys(),key=len, reverse=True)) + r')') #match the keys in the order of their length longest to shortest to avoid repeats
+            converted = pattern.sub(r'err.vars["\1"]', eq)
+
+            print(eq + ' = ' + str(eval(converted))) # don't use eval unless you are the only user (which i am) #fix later
+
+        except Exception as e:
+            print(str(e) + ": " + str(converted))
+    main()#recursive call so you can go back and change variables
+
+
 if __name__ == "__main__":
-    test = err(15, .5)
-    test2 = err(20, .5)
-
-    d=err(120, 3)
-    t=err(20.0, 1.2)
-    print("Velocity:", d/t)
-
-    w=err(4.52, 0.02)
-    x=err(2.0, 0.02)
-    y=err(3.0, .6)
-    print("product and power", w*x+y**2)
+    main()
